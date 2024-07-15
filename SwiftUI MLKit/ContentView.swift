5c144389fb68f72502774dd47a990277728ba7fb
Narayan Shettigar <narayan.p@cumualtions.com>
2024-07-15 16:37:48 +0530

    Initial Commit

---
+++ SwiftUI MLKit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI MLKit
+//
+//  Created by Narayan Shettigar on 15/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI MLKit/SwiftUI_MLKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MLKitApp.swift
+//  SwiftUI MLKit
+//
+//  Created by Narayan Shettigar on 15/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MLKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
